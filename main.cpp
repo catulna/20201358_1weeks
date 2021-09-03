47bc4d0ad055ad62b2470624f502e4d0799f0b64
이나영 <70675003+catulna@users.noreply.github.com>
2021-09-03 02:59:13 +0000

    덧셈

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 
 int main(int argc, char* args[])
 {
-  printf("Hello SDL\n");
+  int a = 0, b = 0;
 
+  printf("두 개의 정수를 입력하세요. \n");
+  scanf("%d %d", &a, &b);
+
+  printf("두 수의 합: %d", a+b);
+ 
   return 0;
 }
