e32e79143f0638279e59053ba8ac158cf73117d7
이나영 <70675003+catulna@users.noreply.github.com>
2021-09-10 03:25:24 +0000

    윈도우 창 띄우기

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
+#include <SDL.h>
 #include <stdio.h>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
-  int a = 0, b = 0;
+  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow("Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 240, SDL_WINDOW_SHOWN);
+    //사이즈 변경. 640, 480 -> 320, 240
 
-  printf("두 개의 정수를 입력하세요. \n");
-  scanf("%d %d", &a, &b);
+    if(g_pWindow != 0)
+    {
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+    }
+  }
+  else
+  {
+    return 1;
+  }
 
-  printf("두 수의 합: %d", a+b);
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 103, 163, 255); 
+  //RGB값 변경. 0, 0, 0 검정색 -> 181, 227, 216 민트색 ->  0, 103, 163 파랑색
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+
+  SDL_Delay(5000);
+  SDL_Quit();
  
   return 0;
 }
