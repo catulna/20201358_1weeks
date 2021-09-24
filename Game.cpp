1572c7a31bfc3862658fb9c806a606e8696017ad
이나영 <70675003+catulna@users.noreply.github.com>
2021-09-24 02:27:55 +0000

    애니메이션 스프라이트2 (png)

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "SDL_image.h"
 
 
 
@@ -13,7 +14,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 
       if(m_pRenderer != 0)
       {
-        SDL_SetRenderDrawColor(m_pRenderer, 74, 168, 216, 255);
+        SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
       }
       else 
       {
@@ -29,7 +30,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
     return false;
   }
 
-  SDL_Surface* pTempSurFace = SDL_LoadBMP("Assets/animate.bmp");
+  SDL_Surface* pTempSurFace = IMG_Load("Assets/animate.png");
 
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurFace);
   SDL_FreeSurface(pTempSurFace);
