e2ec8c7f4f724599c645a21e9b7011325e71ebb6
이나영 <70675003+catulna@users.noreply.github.com>
2021-10-15 05:03:16 +0000

    주석 처리한 코드 정리 변경된 코드는 없음

--- Game.cpp
+++ Game.cpp
@@ -30,32 +30,12 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
     return false;
   }
 
-  /*SDL_Surface* pTempSurFace = IMG_Load("Assets/animate-alpha.png"); 
-
-  m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurFace);
-  SDL_FreeSurface(pTempSurFace);
-
-  
-  m_sourceRectangle.x = 0;
-  m_sourceRectangle.y = 0;
-
-  m_destinationRectangle.w = m_sourceRectangle.w = 128;
-  m_destinationRectangle.h = m_sourceRectangle.h = 82;
-
-
-
-  m_destinationRectangle.x = 100;
-  m_destinationRectangle.y = 100;*/
-
-  //m_textureManager.load("Assets/animate-alpha.png", "animate", m_pRenderer);
 
   if( !TextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer))
   {
     return false;
   }
   
-  //m_go.load(100, 100, 128, 82, "animate");
-  //m_player.load(300, 300, 128, 82, "animate");
 
   GameObject* m_go = new GameObject();
   GameObject* m_player = new Player();
@@ -73,15 +53,6 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 void Game::render()
 {
   SDL_RenderClear(m_pRenderer);
-  //SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
-  //SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 45, 0, SDL_FLIP_HORIZONTAL); //반대로
-  //SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 30, 0, SDL_FLIP_VERTICAL); //뒤집기
-  //m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
-  //m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
-  //m_textureManager.drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
-
-  //TextureManager::Instance()->draw("animate", 0, 50, 128, 82, m_pRenderer);
-  //TextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
 
   for(int i = 0; i < m_gameobjects.size(); i++)
   {
@@ -96,9 +67,7 @@ void Game::update()
 {
   m_currentFrame = ((SDL_GetTicks() / 100) % 6); 
 
-  //m_go.update();
-  //m_player.update();
-   for(int i = 0; i < m_gameobjects.size(); i++)
+  for(int i = 0; i < m_gameobjects.size(); i++)
   {
     m_gameobjects[i]->update();
   }
--- Game.h
+++ Game.h
@@ -20,17 +20,12 @@ class Game{
     void clean();
 
   private:
-    //TextureManager m_textureManager;
+    
     int m_currentFrame;
     SDL_Window* m_pWindow;
     SDL_Renderer* m_pRenderer;
-    //SDL_Texture* m_pTexture;
-    //SDL_Rect m_sourceRectangle;
-    //SDL_Rect m_destinationRectangle;
     bool m_bRunning;
 
-    //GameObject m_go;
-    //Player m_player;
 
     std::vector<GameObject*> m_gameobjects;
 };
